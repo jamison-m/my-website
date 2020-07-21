e18cd7aa59ae0d08d1387c921f8c2017cdb3e9c1
Jamison Miles <jam2016@utexas.edu>
2020-07-20 19:01:39 -0500

    Add files via upload
    First commit, weird issue with trying to do it through command line

---
+++ src/pages/projects.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default class Projects extends Component {
+    render(){
+        return(
+            <div>
+                <h1 className="Projects">Projects</h1>
+                    First large personal project is this website!
+            </div>
+        );
+    }
+}
